3c31c44f5664e40724732cdfe0488f4f3dec524c
cdsjg-iscteiul <56074386+cdsjg-iscteiul@users.noreply.github.com>
2020-12-11 09:29:10 +0000

    teste 123

--- main.py
+++ main.py
@@ -7,48 +7,44 @@ rand.seed(3)
 
 
 def teste():
-    points_x = []
-    points_y = []
-    points_x_2 = []
-    points_y_2 = []
+    points_set1 = []
+    points_set2 = []
 
     mu_1 = (5, 5)
     mu_2 = (-3, -3)
-    print(rand.randint(0, 10))
-    print(rand.randint(0, 10))
     sigma_1 = ((rand.randint(0, 10), rand.randint(0, 5)), (rand.randint(0, 10), rand.randint(0, 5)))
     sigma_2 = ((rand.randint(0, 10), rand.randint(0, 5)), (rand.randint(0, 1), rand.randint(0, 5)))
 
-    for t in range(500):
-        x = num.random.multivariate_normal(mu_1, sigma_1)
-        y = num.random.multivariate_normal(mu_1, sigma_1)
-        points_x.append(x)
-        points_y.append(y)
+    for t in range(100):
+        point_set1 = num.random.multivariate_normal(mu_1, sigma_1)
+        point_set2 = num.random.multivariate_normal(mu_2, sigma_2)
 
-    for b in range(500):
-        x_2 = num.random.multivariate_normal(mu_2, sigma_2, tol=1e-6)
-        y_2 = num.random.multivariate_normal(mu_2, sigma_2, tol=1e-6)
-        points_x_2.append(x_2)
-        points_y_2.append(y_2)
+        points_set1.append(point_set1)
+        points_set2.append(point_set2)
 
-    database = write_to_file(points_x, points_y, points_x_2, points_y_2, "points")
-    for t in range(10):
-        teste12 = changing(database)
+    database = write_to_file(points_set1, points_set2, "points")
+    plot(database)
+
+    for t in range(2):
+        test12 = changing(database)
         if t == 1:
-            write_to_file(teste12, [], [], [], "1st")
+            plotted = write_to_file(test12, [], "1st")
+            print(str(plotted))
+            plot(plotted)
 
 
-def plot(points1, points2, points3, points4):
+def plot(points):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(points1, points2, color="b", marker="s", label="1st set")
-    ax.scatter(points3, points4, color="r", marker="o", label="2nd set")
-    plt.legend(loc='upper left')
+    x = points[:, 0]
+    y = points[:, 1]
+
+    ax.scatter(x, y, color='r')
     plt.show()
 
 
-def write_to_file(points_1, points_2, points_3, points_4, name):
-    all_points = points_1 + points_2 + points_3 + points_4
+def write_to_file(points_1, points_2, name):
+    all_points = points_1 + points_2
 
     f = open(str(name) + ".txt", "w")
     f.write(' '.join(map(str, all_points)))
@@ -57,27 +53,34 @@ def write_to_file(points_1, points_2, points_3, points_4, name):
 
 
 def changing(dataset):
-    result_data = dataset
-    r1 = rand.choice(result_data)
-    r2 = rand.choice(result_data)
+    result_data = []
+    r1 = rand.choice(dataset)
+    r2 = rand.choice(dataset)
     alpha = 10e-7
 
-    for x in dataset:
-        # distance_r1_x = math.sqrt(((r1[0] - x[0]) ** 2) + ((r1[1] - x[1]) ** 2))
-        distance_r1_x = math.hypot((r1[0] - x[0]), (r1[1] - x[1]))
-        # distance_r2_x = math.sqrt(((r2[0] - x[0]) ** 2) + ((r2[1] - x[1]) ** 2))
-        distance_r2_x = math.hypot((r2[0] - x[0]), (r2[1] - x[1]))
+    x = 0
+
+    while x < len(dataset):
+        # print(x)
+        t = dataset[x]
+        distance_r1_x = math.sqrt(((r1[0] - t[0]) ** 2) + ((r1[1] - t[1]) ** 2))
+        # distance_r1_x = math.hypot((r1[0] - x[0]), (r1[1] - x[1]))
+        # print(distance_r1_x)
+        distance_r2_x = math.sqrt(((r2[0] - t[0]) ** 2) + ((r2[1] - t[1]) ** 2))
+        # distance_r2_x = math.hypot((r2[0] - x[0]), (r2[1] - x[1]))
+        # print(distance_r2_x)
 
         if distance_r1_x > distance_r2_x:
-            r1[0] = (1 - alpha) * r1[0] + alpha * x[0]
-            r1[1] = (1 - alpha) * r1[1] + alpha * x[1]
+            r1 = (1 - alpha) * r1 + alpha * x
             result_data.append(r1)
 
-        elif distance_r2_x > distance_r1_x:
-            r2[0] = (1 - alpha) * r2[0] + alpha * x[0]
-            r2[1] = (1 - alpha) * r2[1] + alpha * x[1]
+        if distance_r2_x > distance_r1_x:
+            r2 = (1 - alpha) * r2 + alpha * x
             result_data.append(r2)
 
+        x += 1
+
+    print("SAI DA CENA")
     return result_data
 
 
