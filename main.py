00c264c1096d11f17bcc6797a72adc87545842be
cdsjg-iscteiul <56074386+cdsjg-iscteiul@users.noreply.github.com>
2020-12-21 19:21:43 +0000

    FINAL

--- main.py
+++ main.py
@@ -5,44 +5,92 @@ import numpy as num
 import matplotlib.pyplot as plt
 
 numb_points = 1000
-rand.seed(3)
 
 
-def teste():
-    points_set1 = []
-    points_set2 = []
-    values_r1_r2 = []
+def load_data():
+    data = num.loadtxt("./TwoDistributionsMixed.txt", delimiter=" ")
+    full = [data[10000:], data[:10000]]
+    values_x = []
+    values_y = []
+    ddaata = []
 
-    mu_1 = (5, 5)
-    mu_2 = (-3, -3)
-    sigma_1 = ((rand.randint(0, 10), rand.randint(0, 5)), (rand.randint(0, 10), rand.randint(0, 5)))
-    sigma_2 = ((rand.randint(0, 10), rand.randint(0, 5)), (rand.randint(0, 1), rand.randint(0, 5)))
+    for x in range(10000):
+        values_x.append(full[1][0][x])
+        values_y.append(full[1][1][x])
 
-    for t in range(numb_points):
-        point_set1 = num.random.multivariate_normal(mu_1, sigma_1)
-        point_set2 = num.random.multivariate_normal(mu_2, sigma_2)
+    for g in range(len(values_y)):
+        ddaata.append([values_x[g], values_y[g]])
 
-        points_set1.append(point_set1)
-        points_set2.append(point_set2)
+    return ddaata
 
-    database = write_to_file(points_set1, points_set2, "points")
+
+def run():
+    database = write_to_file(load_data(), "points")
     plot_begin(database)
-    for t in range(2):
+    values_r = []
+    close_r1 = []
+    close_r2 = []
+    for x in range(30):
+        rand.seed(2)
         r1 = rand.choice(database)
-        print("VALOR DO R1:   " + str(r1))
         r2 = rand.choice(database)
-        print("VALOR DO R2:   " + str(r2))
         alpha = 10e-7
-        test12 = changing(database, r1, r2, alpha)
-        test12 = new_change(database, r1, r2, alpha)
-        values_r1_r2.append(test12)
-        print("VALOR DOS R:   " + str(values_r1_r2))
-        if t == 1:
-            plotted = write_to_file(test12, [], "1st")
+        for y in range(10):
+            if y == 1:
+                write_to_file([close_r1 + close_r2], "1st")
+            for g in database:
+                distance_r1_x = math.sqrt(((r1[0] - g[0]) ** 2) + ((r1[1] - g[1]) ** 2))
+                distance_r2_x = math.sqrt(((r2[0] - g[0]) ** 2) + ((r2[1] - g[1]) ** 2))
+
+                if distance_r1_x < distance_r2_x:
+                    r1[0] = (1 - alpha) * r1[0] + alpha * g[0]
+                    r1[1] = (1 - alpha) * r1[1] + alpha * g[1]
+                    close_r1.append([r1[0], r1[1]])
+
+                if distance_r1_x > distance_r2_x:
+                    r2[0] = (1 - alpha) * r2[0] + alpha * g[0]
+                    r2[1] = (1 - alpha) * r2[1] + alpha * g[1]
+                    close_r2.append([r2[0], r2[1]])
+    plot_rs(close_r1, close_r2)
+    deviation = num.std([close_r1 + close_r2])
+    print("VALOR DO DESVIO:  " + str(deviation))
+    plot_normal(close_r1, close_r2)
 
 
-# deviation = num.std(values_r1_r2)
-# print(deviation)
+def run_2():
+    database = write_to_file(load_data(), "points")
+    plot_begin(database)
+    close_r1 = []
+    close_r2 = []
+    for x in range(30):
+        r1 = rand.choice(database)
+        r2 = rand.choice(database)
+        alpha = 10e-7
+        d_x = 0
+        d_y = 0
+        d_x_2 = 0
+        d_y_2 = 0
+
+        for g in database:
+            distance_r1_x = math.sqrt(((r1[0] - g[0]) ** 2) + ((r1[1] - g[1]) ** 2))
+            distance_r2_x = math.sqrt(((r2[0] - g[0]) ** 2) + ((r2[1] - g[1]) ** 2))
+
+            if distance_r1_x < distance_r2_x:
+                d_x += (g[0] - r1[0])
+                d_y += (g[1] - r1[1])
+
+            if distance_r1_x > distance_r2_x:
+                d_x_2 += (g[0] - r2[0])
+                d_y_2 += (g[1] - r2[1])
+
+        r1[0] = (alpha / len(database)) * d_x
+        r1[1] = (alpha / len(database)) * d_y
+        r2[0] = (alpha / len(database)) * d_x_2
+        r2[1] = (alpha / len(database)) * d_y_2
+        close_r1.append(r1)
+        close_r2.append(r2)
+
+    plot_rs(close_r1, close_r2)
 
 
 def plot_close(points_r1, points_r2):
@@ -69,19 +117,44 @@ def plot_close(points_r1, points_r2):
     plt.show()
 
 
-def plot_rs(points):
+def plot_normal(points, points2):
+    x_r1 = []
+    y_r1 = []
+    x_r2 = []
+    y_r2 = []
+    for x in points:
+        x_r1.append(x[0])
+        y_r1.append(x[1])
+
+    for x in points2:
+        x_r2.append(x[0])
+        y_r2.append(x[1])
+
+    fig, ax = plt.subplots(nrows=2, ncols=2)
+    ax0, ax1, ax2, ax3 = ax.flatten()
+    ax0.hist(x_r1)
+    ax0.set_title('Valores X de r1')
+    ax1.hist(y_r1)
+    ax1.set_title('Valores Y de r1')
+    ax2.hist(x_r2)
+    ax2.set_title('Valores X de r2')
+    ax3.hist(y_r2)
+    ax3.set_title('Valores Y de r2')
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_rs(points, points2):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    print(str(points))
-    r1 = points[0]
-    r2 = points[1]
+    x_r1, y_r1 = zip(*points)
+    x_r2, y_r2 = zip(*points2)
 
-    ax.scatter(r1[0], r1[1], color='y', label="r1")
-    ax.scatter(r2[0], r2[1], color='m', label="r2")
-    ax.legend()
-
+    ax.scatter(x_r1, y_r1, color='b', label="r1")
+    ax.scatter(x_r2, y_r2, color='r', label="r2")
     plt.xlim(-15, 10)
     plt.ylim(-10, 10)
+    ax.legend()
     plt.show()
 
 
@@ -93,13 +166,15 @@ def plot_begin(points_r1):
     for x in points_r1:
         x_s_r1.append(x[0])
         y_s_r1.append(x[1])
-    ax.scatter(x_s_r1, y_s_r1, color='b', label="dataset")
+    ax.scatter(x_s_r1, y_s_r1, label="dataset")
     ax.legend()
+    plt.xlim(-15, 12)
+    plt.ylim(-12, 7)
     plt.show()
 
 
-def write_to_file(points_1, points_2, name):
-    all_points = points_1 + points_2
+def write_to_file(points_1, name):
+    all_points = points_1
 
     f = open(str(name) + ".txt", "w")
     f.write(' '.join(map(str, all_points)))
@@ -157,81 +232,4 @@ def closest_point(dataset, point):
     return closest[0]
 
 
-def changing(dataset, r1, r2, alpha):
-    result_data = [r1, r2]
-    rand.seed()
-    close_r1 = []
-    close_r2 = []
-    # print("VALOR ANTES RESULT:   " + str(result_data))
-    x = 0
-    while x < len(dataset):
-        # print(x)
-        t = dataset[x]
-        distance_r1_x = math.sqrt(((r1[0] - t[0]) ** 2) + ((r1[1] - t[1]) ** 2))
-        # distance_r1_x = math.hypot((r1[0] - t[0]), (r1[1] - t[1]))
-        # print(distance_r1_x)
-        distance_r2_x = math.sqrt(((r2[0] - t[0]) ** 2) + ((r2[1] - t[1]) ** 2))
-        # distance_r2_x = math.hypot((r2[0] - t[0]), (r2[1] - t[1]))
-        # print(distance_r2_x)
-
-        if distance_r1_x < distance_r2_x:
-            r1[0] = (1 - alpha) * r1[0] + alpha * t[0]
-            r1[1] = (1 - alpha) * r1[1] + alpha * t[1]
-            # print(r1)
-            result_data[0] = r1
-            # print("RESULT DATA:  " + str(result_data[0]))
-            close_r1.append(t)
-
-        if distance_r1_x > distance_r2_x:
-            r2[0] = (1 - alpha) * r2[0] + alpha * t[0]
-            r2[1] = (1 - alpha) * r2[1] + alpha * t[1]
-            result_data[1] = r2
-            close_r2.append(t)
-
-        x += 1
-    # print("VALOR DEPOIS RESULT:   " + str(result_data))
-    plot_rs(result_data)
-    plot_close(close_r1, close_r2)
-    return result_data
-
-
-def new_change(dataset, r1, r2, alpha):
-    result_data = []
-    d_x = 0
-    d_y = 0
-    d_x_2 = 0
-    d_y_2 = 0
-    close_r1 = []
-    close_r2 = []
-    for g in dataset:
-        distance_r1_x = math.hypot((r1[0] - g[0]), (r1[1] - g[1]))
-        distance_r2_x = math.hypot((r2[0] - g[0]), (r2[1] - g[1]))
-
-        if distance_r1_x < distance_r2_x:
-            d_x += (g[0] - r1[0])
-            d_y += (g[1] - r1[1])
-            close_r1.append(g)
-
-        if distance_r1_x > distance_r2_x:
-            d_x_2 += (g[0] - r2[0])
-            d_y_2 += (g[1] - r2[1])
-            close_r2.append(g)
-
-        r1[0] = (alpha / len(dataset)) * d_x
-        r1[1] = (alpha / len(dataset)) * d_y
-        r2[0] = (alpha / len(dataset)) * d_x_2
-        r2[1] = (alpha / len(dataset)) * d_y_2
-
-    result_data.append(r1)
-    result_data.append(r2)
-    plot_close(close_r1, close_r2)
-
-    # print(str(result_data))
-    return result_data
-
-
-def main_run():
-    teste()
-
-
-main_run()
+run()
